f5f748c1a39275eb5bb1c734db50896195aaed49
Haadt <55682433+HaaDT@users.noreply.github.com>
2022-06-29 04:19:26 -0400

    Add files via upload

--- src/main/java/dtauxiliary/DynamicTreesAuxiliaryBiomes.java
+++ src/main/java/dtauxiliary/DynamicTreesAuxiliaryBiomes.java
@@ -1,15 +1,14 @@
 package dtauxiliary;
 
 import com.ferreusveritas.dynamictrees.ModConstants;
+import com.ferreusveritas.dynamictrees.api.WorldGenRegistry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid=DynamicTreesAuxiliaryBiomes.MOD_ID, name = DynamicTreesAuxiliaryBiomes.NAME, version = DynamicTreesAuxiliaryBiomes.VERSION, dependencies = DynamicTreesAuxiliaryBiomes.DEPENDENCIES, updateJSON = "")
+@Mod(modid=DynamicTreesAuxiliaryBiomes.MOD_ID, name = DynamicTreesAuxiliaryBiomes.NAME, version = DynamicTreesAuxiliaryBiomes.VERSION, dependencies = DynamicTreesAuxiliaryBiomes.DEPENDENCIES)
+@Mod.EventBusSubscriber(modid = DynamicTreesAuxiliaryBiomes.MOD_ID)
 public final class DynamicTreesAuxiliaryBiomes {
 
     public static final String MOD_ID = "dtauxiliary";
@@ -17,29 +16,11 @@ public final class DynamicTreesAuxiliaryBiomes {
     public static final String VERSION = "@VERSION@";
     public static final String DEPENDENCIES = ModConstants.REQAFTER + ModConstants.DYNAMICTREES_LATEST + ModConstants.NEXT + ModConstants.REQAFTER + "auxbiomes";
 
-    @Mod.Instance
-    public static DynamicTreesAuxiliaryBiomes instance;
-
     public static final Logger logger = LogManager.getLogger(MOD_ID);
 
-    @SidedProxy(clientSide = "dtauxiliary.ClientProxy", serverSide = "dtauxiliary.CommonProxy")
-    public static CommonProxy proxy;
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit();
+    @SubscribeEvent
+    public static void registerDataBasePopulators(final WorldGenRegistry.BiomeDataBasePopulatorRegistryEvent event) {
+        event.register(new BiomeDataBasePopulator());
     }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        proxy.init();
-    }
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-        proxy.postInit();
-    }
-
- 
 
 }
